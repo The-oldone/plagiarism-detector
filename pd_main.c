f3f04cb4bd931724858db3fe9aaecc0f7b332703
fearwalia <139103754+The-oldone@users.noreply.github.com>
2024-10-04 13:10:21 +0530

    basic file i/o testing

--- pd_main.c
+++ pd_main.c
@@ -1,7 +1,24 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"pd.h"
+#include<string.h>
 
-int main(void) {
-	
+int main(int argc, char *argv[]) {
+	FILE *f1 = fopen(argv[1], "r"), *f2 = fopen(argv[2], "r");
+	char word[64];
+	if(!f1) {
+		printf("first file doesn't exist\n");
+		return 1;
+	} else if(!f2) {
+		printf("second file doesn't exist\n");
+		return 1;
+	}
+	while(!feof(f1)){
+		fscanf(f1, "%s", word);
+		queuePush(word);
+	}
+	printf("reading done\n");
+	fclose(f1);
+	fclose(f2);
+	return 0;
 }
